80143b57f6d5de52a8686eda22553093ab85ccab
Francisco Cheda Pérez <franciscochedap@gmail.com>
2021-10-13 20:18:28 +0200

    code updates to fix minor errors

--- example.py
+++ example.py
@@ -8,23 +8,22 @@ import numpy as np
 
 if __name__ == '__main__':
 
-    ## LOAD DATA
+    # LOAD DATA
     (x_train, y_train), (x_test, y_test) = mnist.load_data()
     x_train = np.expand_dims(x_train, axis=-1)
     x_test = np.expand_dims(x_test, axis=-1)
     y_train = to_categorical(y_train)
     y_test = to_categorical(y_test)
 
-    ## LOAD MODEL AND COMPILE IT (NEVER FORGET TO COMPILE!)
+    # LOAD MODEL AND COMPILE IT (NEVER FORGET TO COMPILE!)
     model = wrn164(input_shape=x_train.shape[1:], nclasses=10, regularization=5e-4)
     model.compile(optimizer='sgd', lr=1e-1, metrics=['acc'], loss='categorical_crossentropy')
 
-    ## LOAD E2EFS AND RUN IT
-    fs_class = e2efs.E2EFSSoft(n_features_to_select=39).attach(model).fit(
-        x_train, y_train, batch_size=128, validation_data=(x_test, y_test), verbose=2
-    )
+    # LOAD E2EFS AND RUN IT
+    fs_class = e2efs.E2EFSSoft(n_features_to_select=39).attach(model)
+    fs_class.fit(x_train, y_train, batch_size=128, validation_data=(x_test, y_test), verbose=2)
 
-    ## FINE TUNING
+    # FINE TUNING
     def scheduler(epoch):
         if epoch < 20:
             return .1
@@ -40,4 +39,3 @@ if __name__ == '__main__':
     print('FEATURE_RANKING :', fs_class.get_ranking())
     print('ACCURACY : ', fs_class.get_model().evaluate(x_test, y_test, batch_size=128)[-1])
     print('FEATURE_MASK NNZ :', np.count_nonzero(fs_class.get_mask()))
-
